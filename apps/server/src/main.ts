632b55f8dfed500917eb3b3ddd6ed9f15c373527
Kaiyang Zheng <kaiyang.zheng@gmail.com>
2024-05-04 19:58:09 -0400

    configure openapi client

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,12 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TrpcRouter } from '@server/trpc/trpc.router';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('NUStudy API')
+    .setDescription('The NUStudy API description')
+    .setVersion('1.0')
+    .addTag('NUStudy')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const trpc = app.get(TrpcRouter);
   trpc.applyMiddleware(app);
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 8080);
 }
+
 bootstrap();
